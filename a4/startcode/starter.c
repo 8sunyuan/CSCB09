00f9b6c6144a75198ac987c9ba75dca87618b2d2
Michael <michaelyuan.sun@mail.utoronto.ca>
2018-04-05 11:35:58 -0400

    Fixed next_turn and player name handling. Still need to fix game breaking from player disconnecting

--- a4/startcode/starter.c
+++ a4/startcode/starter.c
@@ -31,7 +31,6 @@ extern void makelistener();
 extern int compute_average_pebbles();
 extern int game_is_over();  /* boolean */
 extern void broadcast(char *s);
-extern void print_board();
 
 void delete(int fd)
 {
@@ -56,11 +55,13 @@ void insert(int fd, int playing)
         fprintf(stderr, "out of memory!\n");  /* unlikely */
         exit(1);
     }
+    strcpy(new->name, "");
     new->fd = fd;
     new->playing = playing;
     // Set pebbles for each pit
     for (i = 0; i < NPITS; i++)
         new->pits[i] = pebbles;
+    new->pits[NPITS] = 0;
     //Insert at head of list
     new->next = playerlist;
     playerlist = new;
@@ -70,7 +71,7 @@ void printall()
 {
     struct player *p;
     for (p = playerlist; p; p = p->next)
-        printf("%d: %d\n", p->fd, p->playing);
+        printf("NAME: %s (%d: %d)\n", p->name, p->fd, p->playing);
     printf("[end]\n");
 }
 
@@ -96,6 +97,37 @@ int is_valid(char *s) {
     return 0;
 }
 
+void print_board(int turn) {
+    struct player *p = playerlist;
+    char message[1025] = "";
+    char move[MAXNAME + 20];
+    // Get string for board
+    for (p = playerlist; p; p = p->next) {
+        if (p->playing) {
+            sprintf(message, "%s%s: ", message, p->name);
+            for (int i = 0; i <= NPITS; i++) {
+                if (i < NPITS)
+                    sprintf(message, "%s[%d]", message, i);
+                else
+                    sprintf(message, "%s[end pit]", message);
+                sprintf(message, "%s%d ", message, p->pits[i]);
+            }
+            strcat(message, "\n");
+        }
+        if (turn == p->fd)
+            sprintf(move, "It is %s's turn\n", p->name);
+    }
+    // Send board message to players
+    for (p = playerlist; p; p = p->next) {
+        if (p->playing == 0) continue;
+        send(p->fd, message, strlen(message), 0);
+        if (turn == p->fd)
+            send(p->fd, "Your move\n", 11, 0);
+        else
+            send(p->fd, move, strlen(move), 0);
+    }
+}
+
 char *trimwhitespace(char *str)
 {
   char *end;
@@ -115,24 +147,30 @@ char *trimwhitespace(char *str)
   return str;
 }
 
-// int parse_move(int fd, char *s) {
-//     buf[0] = trimwhitespace(buf);
-//     char *endptr;
-//     int pitnum = strtol(buf, &endptr, 10);
-//     if (*endptr != '\0') break;
-//     int pebbles;
-//     if ((pebbles = p->pits[pitnum]) == 0) {
-//         strcpy(message, "Your pit number ");
-//         strcat(message, itoa(pitnum));
-//         strcat(message, " is empty\n");
-//         send(p->fd, message, strlen(message), 0);
-//         break;
-//     }
-// }
+int next_turn(struct player *p, int turn, int disconnect) {
+    struct player *pp;
+    pp = p;
+    do
+        pp = (pp->next) ? pp->next : playerlist;
+    while(!pp->playing);
+    // Went through whole list and no one else playing
+    // if player p is disconnecting then it's no one's turn
+    if (disconnect && pp->fd == turn) {
+        printf("No one's turn\n");
+        turn = -1;
+    } else {
+        turn = pp->fd;
+        //printf("%s's turn now at %d \n", pp->name, turn);
+    }
+    printf("Name:%s FD:%d NEW TURN:%d\n\n", p->name, p->fd, turn);
+    return turn;
+}
+
+
 
 int main(int argc, char **argv)
 {
-    struct player *p, *next;
+    struct player *p, *q;
     fd_set readfds;
     char msg[MAXNAME + 50];
     int server_socket, new_socket;
@@ -194,11 +232,10 @@ int main(int argc, char **argv)
                 perror("send");
             // Create new player and add to end of linked list
             // Currently not playing, has to input their name but is connected
-            if (playerlist == NULL)
-                turn = new_socket;
             insert(new_socket, 0);
             continue;
         }
+
         // IO operation on other socket
         //printall();
         for (p = playerlist; p; p = p->next) {
@@ -208,29 +245,29 @@ int main(int argc, char **argv)
                     //Somebody disconnected , get his details and print
                     getpeername(p->fd , (struct sockaddr*)&r , (socklen_t*)&size);
                     printf("disconnecting client ip %s\n" , inet_ntoa(r.sin_addr));
-                    strcpy(message, p->name);
-                    strcat(message, " has left the game.\n");
+                    // strcpy(message, p->name);
+                    // strcat(message, " has left the game.\n");
+                    sprintf(message, "%s has left the game\n", p->name);
                     broadcast(message);
-                    //Close the socket and mark as 0 in list for reuse
-                    next = p->next;
-                    // Change players move
-                    if (turn == p->fd) {
-                        if (next != NULL) {
-                            turn = next->fd;
-                        } else if (playerlist != NULL) {
-                            turn = playerlist->fd;
-                        } else {   // Probably don't need this line but whatever
-                            turn = -1;
-                            printf("No one's turn\n");
+                    if ((turn = next_turn(p, turn, 1)) != -1) {
+                        for (q = playerlist; q; q = q->next) {
+                            if (q->fd == turn)
+                                sprintf(message, "It is %s's turn\n", q->name);
                         }
                     }
+                    print_board(turn);
+                    broadcast(message);
                     close(p->fd);
+                    // CHECK DELETE
+                    printf("No SEG HERE\n");
                     delete(p->fd);
+                    printall();
                 }
                 buf[valread] = '\0';
                 // printf("BUF is %s\n", buf);
                 // Player isn't playing and needs to put in a name first
                 if (!(p->playing)) {
+                    printall();
                     // Make newline terminating zero byte. Don't want two newlines
                     lastchar = buf[valread-1];
                     buf[valread-1] = '\0';
@@ -247,15 +284,19 @@ int main(int argc, char **argv)
                         send(p->fd, "What is your name?\n", 19, 0);
                         break;
                     // Valid name
-                    } else if (lastchar == '\r' || lastchar == '\n') {
-                        p->playing = 1;
-                        strcpy(p->name, buf);
-                        strcpy(message, buf);
-                        strcat(message, " has joined the game\n");
-                        printf("%s's name is set to %s\n", inet_ntoa(r.sin_addr), p->name);
-                        broadcast(message);
-                        print_board(turn);
+                    } else {
+                        strcat(p->name, buf);
+                        if (lastchar == '\r' || lastchar == '\n') {
+                            printf("%s's name is set to %s\n", inet_ntoa(r.sin_addr), p->name);
+                            sprintf(message, "%s has joined the game\n", p->name);
+                            broadcast(message);
+                            p->playing = 1;
+                            if (turn == -1)
+                                turn = p->fd;
+                            print_board(turn);
+                        }
                     }
+                    printall();
                 // Player is playing, actual game content here
                 } else {
                     // Not the players move
@@ -263,8 +304,6 @@ int main(int argc, char **argv)
                         send(p->fd, "It is not your move.\n", 22, 0);
                         break;
                     }
-                    turn = (p->next != NULL) ? p->next->fd : playerlist->fd;
-                    //printf("%s's turn. turn = %d, p->fd = %d", p->name, turn, p->fd);
                     // Trim trailing space
                     char *end = buf + strlen(buf) - 1;
                     while(end > buf && isspace((unsigned char)*end)) end--;
@@ -281,28 +320,31 @@ int main(int argc, char **argv)
                         }
                         p->pits[pitnum] = 0;
                         pitnum++;
+                        q = p;
                         // Distribute pebbles to adjacent pits
                         while (pebbles != 0) {
-                            while (pitnum <= NPITS && p->playing) {
-                                p->pits[pitnum]++;
-                                pitnum++;
+                            while (pitnum <= NPITS && q->playing) {
+                                q->pits[pitnum]++;
                                 if (--pebbles == 0) break;
+                                pitnum++;
                             }
-                            if (pebbles != 0) pitnum = 0;
-                            // Go to next player to distribute pebbles
-                            p = (p->next) ? p->next : playerlist;
+                            // Reset index at 0 and find adjacent player
+                            if (pebbles != 0) {
+                                pitnum = 0;
+                                do {
+                                    q = (q->next) ? q->next : playerlist;
+                                } while(!q->playing);
+                            }
                         }
-                        // If last pebble didn't land in last pit, change turns
-                        if (pitnum == NPITS)
-                            broadcast("DIS BOI GOES AGAIN");
-                        else 
-                            turn = (p->next != NULL) ? p->next->fd : playerlist->fd;
-
+                        // p is the player right now that pit ends on
+                        // If last pebble did land in last pit
+                        if (pitnum == NPITS && q->fd == turn)
+                            broadcast("DIS BOI GOES AGAIN\n");
+                        else
+                            turn = next_turn(p, turn, 0);
                         print_board(turn);
                     } else {
-                        strcpy(message, "Pit number go from 0 to 5\n");
-                        // strcat(message, NPITS + '0');
-                        // strcat(message, " \n");
+                        sprintf(message, "Pit numbers go from 0 to %d\n", NPITS - 1);
                         send(p->fd, message, strlen(message), 0);
                         break;
                     }
@@ -336,41 +378,6 @@ void broadcast_playing(char *s) {
     for (p = playerlist; p; p = p->next) {
         if (p->playing == 0) continue;
         send(p->fd, s, strlen(s), 0);
-    }
-}
-
-void print_board(int turn) {
-    struct player *p = playerlist;
-    char message[1025] = "";
-    char *result;
-    char move[MAXNAME + 20] = "It is ";
-    for (p = playerlist; p; p = p->next) {
-        strcat(message, p->name);
-        strcat(message, ": ");
-        for (int i = 0; i <= NPITS; i++) {
-            result = malloc(8*sizeof(p->pits[i]));
-            sprintf(result, "%d", i);
-            strcat(message, "[");
-            strcat(message, result);
-            strcat(message, "]");
-            sprintf(result, "%d", p->pits[i]);
-            strcat(message, result);
-            strcat(message, " ");
-        }
-        strcat(message, "\n");
-        if (turn == p->fd) {
-            strcat(move, p->name);
-            strcat(move, "'s turn\n");
-        }
-    }
-    
-    for (p = playerlist; p; p = p->next) {
-        if (p->playing == 0) continue;
-        send(p->fd, message, strlen(message), 0);
-        if (turn == p->fd)
-            send(p->fd, "YOUR MOVE?\n", 11, 0);
-        else
-            send(p->fd, move, strlen(move), 0);
     }
 }
 
